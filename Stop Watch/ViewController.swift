97402b09ed724b7c1ba5e5093644a2c549efbb3d
Abshir <abshirm@live.com>
2019-07-06 16:20:08 -0700

    Initial Commit

---
+++ Stop Watch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stop Watch
+//
+//  Created by Abshir Mohamed on 7/6/19.
+//  Copyright © 2019 Abshir Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
