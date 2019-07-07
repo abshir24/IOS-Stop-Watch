24b125ab972fbe9f3575b6f8656725301de6af76
Abshir <abshirm@live.com>
2019-07-07 16:28:10 -0700

    added functionality to buttons

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    var (hours,minutes,seconds,fractions) = (0,0,0,0)
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBAction func Start(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.keepTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func Stop(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func Reset(_ sender: UIButton) {
+        (hours,minutes,seconds,fractions) = (0,0,0,0)
+        
+        time.text = "\(hours):\(minutes):\(seconds)\(fractions)"
+    }
+    
+    func keepTimer(){
+        fractions += 1
+        if fractions > 99 {
+            seconds += 1
+            fractions = 0
+        }
+        if seconds == 60{
+            minutes+=1
+            seconds = 0
+            
+        }
+        if minutes==60{
+            hours+=1
+            minutes = 0
+        }
+        
+        time.text = "\(hours):\(minutes):\(seconds)\(fractions)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
